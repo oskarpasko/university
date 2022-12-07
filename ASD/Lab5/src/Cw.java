65b125d99982f1df0d3edccac3affa88f6bd4f46
Oskar <>
2022-12-07 14:14:32 +0100

    update

--- ASD/Lab5/src/Cw.java
+++ ASD/Lab5/src/Cw.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Cw extends JFrame {
     private JPanel panel1;
@@ -22,5 +24,13 @@ public class Cw extends JFrame {
         this.setContentPane(this.panel1); // wyswietlanie okienka na ekranie
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(400, 400);
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                add okno = new add();
+                okno.setVisible(true);
+            }
+        });
     }
 }
--- ASD/Lab5/src/add.java
+++ ASD/Lab5/src/add.java
@@ -1,2 +1,20 @@
-public class add {
+import javax.swing.*;
+
+public class add extends JFrame {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton button1;
+    private JComboBox comboBox1;
+
+    public static void main(String[] args) {
+        add setPanel = new add();
+        setPanel.setVisible(true);
+    }
+
+    public add() {
+        super("Wbór kursów!");
+        this.setContentPane(this.panel1); // wyswietlanie okienka na ekranie
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(400, 400);
+    }
 }
