db49dad8efa1a0be0b9bb24cfef098d5c2f048c7
Oskar <>
2022-12-09 12:54:14 +0100

    Create Zgadywanie_Liczb_PD.java

---
+++ ASD/kolokwium_1/src/com/company/Zgadywanie_Liczb_PD.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Random;
+
+public class Zgadywanie_Liczb_PD {
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+        int min_num = 0;
+        int max_num = 1000;
+
+        int random_number = rand.nextInt(max_num+1);
+
+        if()
+
+    }
+}
