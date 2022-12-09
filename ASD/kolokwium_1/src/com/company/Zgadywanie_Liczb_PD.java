b207739d6a817c70ff3e9641f1faa104adc61c05
Oskar <>
2022-12-09 12:55:57 +0100

    Update Zgadywanie_Liczb_PD.java

--- ASD/kolokwium_1/src/com/company/Zgadywanie_Liczb_PD.java
+++ ASD/kolokwium_1/src/com/company/Zgadywanie_Liczb_PD.java
@@ -4,14 +4,33 @@ import java.util.Random;
 
 public class Zgadywanie_Liczb_PD {
     public static void main(String[] args) {
-        Random rand = new Random();
+        // write your code here
+        Random random = new Random();
 
-        int min_num = 0;
-        int max_num = 1000;
+        //deklaracja zmiennych: poczatek przedziłu, koniec przedziału, połowa przedziąłu
+        int min_num = 0, max_num = 1000, half = 0;
+        //losowanie liczby od 0 do 1000
+        int random_num = random.nextInt(1001);
 
-        int random_number = rand.nextInt(max_num+1);
+        System.out.println("Liczba do zgadnięcia: " + random_num);
 
-        if()
+        while(true)
+        {
+            //wzrór zwracający zawsze połowe przedziału
+            half = ((max_num - min_num)/2)+min_num;
 
+            if(half > random_num)
+            {
+                System.out.println("Liczba " +half+" jest za duża!");
+                max_num = half-1;
+            }else if(half < random_num)
+            {
+                System.out.println("Liczba " +half+ " jest za mała!");
+                min_num = half+1;
+            }else{
+                System.out.println("Zgadłeś, to liczba: " + half);
+                break;
+            }
+        }
     }
 }
