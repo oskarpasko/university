0e3d8c452f2f478308a32a048f4a8791ed7e5531
Oskar <>
2022-12-27 18:05:01 +0100

    Update MainPage.java

--- PO/Semestr3/OwnProjects/Tests/src/SwingLog/MainPage.java
+++ PO/Semestr3/OwnProjects/Tests/src/SwingLog/MainPage.java
@@ -1,36 +1,19 @@
 package SwingLog;
 
 import javax.swing.*;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.sql.Time;
-
 
 public class MainPage extends JFrame{
     private JPanel panel1;
-    public JLabel label1;
+    public  JLabel label1;
     private JTextField textField1;
 
-
-    public static void main(String[] args) {
-        MainPage panel = new MainPage();
-        panel.setVisible(true);
-    }
-
-    public MainPage() {
+    public MainPage(String str) {
         super("Main Page");
         this.setContentPane(this.panel1); // wyswietlanie okienka na ekranie
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(300, 300);
 
-
-        try {
-            label1.setText(Files.readAllLines(Paths.get("zalogowany.txt")).get(0));
-            textField1.setText(Files.readAllLines(Paths.get("zalogowany.txt")).get(1));
-        }catch (IOException f) {
-            System.out.println("An error occurred!");
-            f.printStackTrace();
-        }
+        String id = str;
+        label1.setText(id);
     }
 }
