d332ba6bf42639fcbc0873865ff04bc090c4d5bf
Oskar <>
2023-05-08 10:21:19 +0200

    cos

--- 4semestr/MetodyNumeryczne/Simpsona_prosty.py
+++ 4semestr/MetodyNumeryczne/Simpsona_prosty.py
@@ -1,26 +1,53 @@
 from sympy import *
 
-x = symbols('x') # symbole
-f = x**2 # wzor funkcji
-d_first = f.diff(x) # 1. pochodna
-d_second = d_first.diff(x) # 2. pochodna
-d_third = d_second.diff(x) # 3. pochodna
-d_fourth = d_third.diff(x) # 4. pochodna
+def simp_prosty():
+    x = symbols('x') # symbole
+    f = x**2 # wzor funkcji
+    d_first = f.diff(x) # 1. pochodna
+    d_second = d_first.diff(x) # 2. pochodna
+    d_third = d_second.diff(x) # 3. pochodna
+    d_fourth = d_third.diff(x) # 4. pochodna
 
-a = -1
-b = 1
+    a = -1
+    b = 1
 
-if(a>b): max = a
-else: max = b
+    if(a>b): max = a
+    else: max = b
 
-fa = f.evalf(subs={x:a})
-fb = f.evalf(subs={x:b})
-c = (a+b)/2
-fc = f.evalf(subs={x:c})
-f4 = d_fourth.evalf(subs={x:max})
+    fa = f.evalf(subs={x:a})
+    fb = f.evalf(subs={x:b})
+    c = (a+b)/2
+    fc = f.evalf(subs={x:c})
+    f4 = d_fourth.evalf(subs={x:max})
 
-s = ((b-a)/6) * (fa+fb + 4 * c)
-b = (1/90) * (pow((b-a), 5) * f4)
+    s = ((b-a)/6) * (fa+fb + 4 * c)
+    b = (1/90) * (pow((b-a), 5) * f4)
 
-print(f"S = {s}")
-print(f"B = {b}")
+    print(f"S = {s}")
+    print(f"B = {b}")
+
+def simp_zlozony():
+    x = symbols('x') # symbole
+    f = x**2 # wzor funkcji
+    d_first = f.diff(x) # 1. pochodna
+    d_second = d_first.diff(x) # 2. pochodna
+    d_third = d_second.diff(x) # 3. pochodna
+    d_fourth = d_third.diff(x) # 4. pochodna
+
+    a = -1
+    b = 1
+
+    if(a>b): max = a
+    else: max = b
+
+    fa = f.evalf(subs={x:a})
+    fb = f.evalf(subs={x:b})
+    c = (a+b)/2
+    fc = f.evalf(subs={x:c})
+    f4 = d_fourth.evalf(subs={x:max})
+
+    s = ((b-a)/6) * (fa+fb + 4 * c)
+    b = (1/90) * (pow((b-a), 5) * f4)
+
+    print(f"S = {s}")
+    print(f"B = {b}")
--- 4semestr/MetodyNumeryczne/trapezow_prostych.py
+++ 4semestr/MetodyNumeryczne/trapezow_prostych.py
@@ -1,22 +1,53 @@
 from sympy import *
 
-x = symbols('x') # symbole
-f = x**2 # wzor funkcji
-d_first = f.diff(x) # 1. pochodna
-d_second = d_first.diff(x) # 2. pochodna
+def trapez_prosty():
+    x = symbols('x') # symbole
+    f = x**2 # wzor funkcji
+    d_first = f.diff(x) # 1. pochodna
+    d_second = d_first.diff(x) # 2. pochodna
 
-a = -1
-b = 1
+    a = -1
+    b = 1
 
-if(a>b): max = a
-else: max = b
+    if(a>b): max = a
+    else: max = b
 
-fa = f.evalf(subs={x:a})
-fb = f.evalf(subs={x:b})
-f2 = d_second.evalf(subs={x:max})
+    fa = f.evalf(subs={x:a})
+    fb = f.evalf(subs={x:b})
+    f2 = d_second.evalf(subs={x:max})
 
-s = ((b-a)/2) * (fa+fb)
-b = (1/12)*pow((b-a), 3) * f2
+    s = ((b-a)/2) * (fa+fb)
+    b = (1/12)*pow((b-a), 3) * f2
 
-print(f"S = {s}")
-print(f"B = {b}")
+    print(f"S = {s}")
+    print(f"B = {b}")
+
+def trapez_zlozony():
+    x = symbols('x') # symbole
+    f = x**2 # wzor funkcji
+    d_first = f.diff(x) # 1. pochodna
+    d_second = d_first.diff(x) # 2. pochodna
+
+    a = -1
+    b = 1
+    n = 5
+
+    h = (b-a)/n
+
+    if(a>b): max = a
+    else: max = b
+
+    fa = f.evalf(subs={x:a})
+    fb = f.evalf(subs={x:b})
+    f2 = d_second.evalf(subs={x:max})
+    suma=0
+
+    for k in range(1, (n-1)):
+        suma += f.evalf(subs={x:k})
+
+    funkcja = h * (((fa+fb)/2) + suma)
+
+    print(f"Funkcja = {funkcja}")
+    print(f"B = {b}")
+
+trapez_zlozony()
