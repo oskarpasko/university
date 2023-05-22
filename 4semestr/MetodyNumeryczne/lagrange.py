99130b92f759ee21a5ebc65e877f45d7ef3baf55
Oskar <oskarpasko@gmail.com>
2023-05-22 11:09:52 +0200

    Update lagrange.py

--- 4semestr/MetodyNumeryczne/lagrange.py
+++ 4semestr/MetodyNumeryczne/lagrange.py
@@ -2,17 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import lagrange
 
-f = lambda x: 1 / (5 + x*x)
-
 # mamy 4 punktów w zakresie -1...2 i obliczamy dla nich f(x)
 x1 = np.linspace(-1, 2, 4)
 y1 = [3, 1, 2, -6]
-
 # wyznaczamy współczynniki 
 F = lagrange(x1, y1)
-
-# wyliczamy wartość funkcji F(x) dla "gęściej" rozmieszczonych punktów
-x2 = np.linspace(-1, 2, 4)
-y2 = F(x2)
 
 print(f'{F}')
