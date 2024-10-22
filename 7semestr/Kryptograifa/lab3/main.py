e4701a63813382098e09ce234453e7296c70d69a
Oskar <oskarpasko@gmail.com>
2024-10-22 12:03:40 +0200

    Update main.py

--- 7semestr/Kryptograifa/lab3/main.py
+++ 7semestr/Kryptograifa/lab3/main.py
@@ -3,8 +3,8 @@ import random
 alf = 'abcdefghijklmnopqrstuvwxyz'
 key2 = ''.join(random.sample(alf, len(alf)))
 
-print(alf, len(alf))
-print(key2)
+print("Alphabet:", alf)
+print("Key:", key2)
 
 def encrypt(tj, klucz, alf):
     slownik = {c: klucz[i] for i, c in enumerate(alf)}
@@ -12,10 +12,25 @@ def encrypt(tj, klucz, alf):
     for c in tj:
         c_lower = c.lower()
         if c_lower in slownik:
-            ct += slownik[c_lower] 
+            ct += slownik[c_lower]
         else:
             ct += c
     return ct
 
-encrypted_text = encrypt('Jakis tam tekst', key2, alf)
-print(encrypted_text)
+def decrypt(ct, klucz, alf):
+    reverse_slownik = {klucz[i]: c for i, c in enumerate(alf)}
+    decrypted_text = ''
+    for c in ct:
+        c_lower = c.lower()
+        if c_lower in reverse_slownik:
+            decrypted_text += reverse_slownik[c_lower]
+        else:
+            decrypted_text += c
+    return decrypted_text
+
+text_to_encrypt = 'Jakis tam tekst'
+encrypted_text = encrypt(text_to_encrypt, key2, alf)
+print("Encrypted:", encrypted_text)
+
+decrypted_text = decrypt(encrypted_text, key2, alf)
+print("Decrypted:", decrypted_text)
